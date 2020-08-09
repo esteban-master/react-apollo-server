4da371fe21ce585050e1dd154379867307734810
Esteban Beltran <estebanenriquebeltran@gmail.com>
2020-08-09 14:57:18 -0400

    Mongo conectado con mongoose
    Resolve #1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const { ApolloServer } = require("apollo-server");
 const resolvers = require("./graphql/resolvers");
 const typeDefs = require("./graphql/schemas");
 
+const connectDB = require("./config/db");
+connectDB();
+
 const server = new ApolloServer({
   resolvers,
   typeDefs,
