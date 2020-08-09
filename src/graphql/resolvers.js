cf18c06d208fea74bae1c6e6aa52e2e593acafa5
Esteban Beltran <estebanenriquebeltran@gmail.com>
2020-08-09 15:11:24 -0400

    Modelo Usuario Mongoose y Graphql
    See: #3

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,6 +1,22 @@
+const Usuario = require("../models/Usuario");
+
 const resolvers = {
   Query: {
     obtenerCurso: () => "Holaaaa",
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { usuario }) => {
+      const { email } = usuario;
+      const findUsuario = await Usuario.findOne({ email });
+      if (findUsuario) {
+        throw new Error("Usuario ya registrado");
+      }
+      try {
+        return new Usuario(usuario).save();
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
 
--- src/graphql/schemas.js
+++ src/graphql/schemas.js
@@ -12,6 +12,17 @@ const typeDefs = gql`
     password: String
     creado: String
   }
+
+  input UsuarioInput {
+    nombre: String!
+    apellido: String!
+    email: String!
+    password: String!
+  }
+
+  type Mutation {
+    nuevoUsuario(usuario: UsuarioInput): Usuario
+  }
 `;
 
 module.exports = typeDefs;
